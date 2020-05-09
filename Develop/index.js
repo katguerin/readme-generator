1990d2cd9a4812bf9995bdf20f4b87c48d4e6c01
katguerin <katguerin55@gmail.com>
2020-05-09 16:18:44 -0700

    added prompts. changing them to fit the readme-challenge

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,98 @@
-// array of questions for user
-const questions = [
+const inquirer = require('inquirer');
 
-];
+const promptUser = () => {
+    return inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project? (required)',
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log('please enter your projects name');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'please enter your github username'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'would you like to enter some information about yourself for an "about section"?',
+            default: true
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'provide some information about yourself:',
+            when: ({ confirmAbout }) => confirmAbout
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'what is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'what did you this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'enter the github link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if(projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    })
+}
+  promptUser()
+  .then(promptProject)
+  .then(portfolioData => {
+    const pageHTML = generatePage(portfolioData);
+  });
+
+
 
 // function to write README file
 function writeToFile(fileName, data) {
